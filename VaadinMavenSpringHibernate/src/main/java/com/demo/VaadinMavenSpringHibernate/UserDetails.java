f8e1051d099486d7b489151093b215b0e3400dd0
OlgaE <olga.efimova.aol@gmail.com>
2013-10-19 19:56:31 +0700

    Annotations: @Entity (name="userdetailes"), @Id, @Column(name="userId").

--- VaadinMavenSpringHibernate/src/main/java/com/demo/VaadinMavenSpringHibernate/UserDetails.java
+++ VaadinMavenSpringHibernate/src/main/java/com/demo/VaadinMavenSpringHibernate/UserDetails.java
@@ -1,13 +1,17 @@
 package com.demo.VaadinMavenSpringHibernate;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
-@Entity   // Now Hibernate knows that this class needs to be saved.
+@Entity (name = "New_User_Details")  // Now Hibernate knows that this class needs to be saved.
 public class UserDetails {
 
 	@Id  // The value of this field will be the primary key.
+	@Column (name = "NewId")
 	private int userId;
+	
+	@Column (name = "NewUserName")
 	private String userName;
 	
 	public int getUserId() {
@@ -16,6 +20,7 @@ public class UserDetails {
 	public void setUserId(int userId) {
 		this.userId = userId;
 	}
+	
 	public String getUserName() {
 		return userName;
 	}
